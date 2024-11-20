fba2fa00aaad5e3e032e9d9dde5d2cfea65b7f12
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2024-11-20 17:24:49 +0100

    finished buttons and products

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,82 @@
+// Styling
 import './App.css'
 
+// Assets
+import bag1 from './assets/bag_1.png'
+import bag2 from './assets/bag_2.png'
+import bag3 from './assets/bag_3.png'
+import bag4 from './assets/bag_4.png'
+import brand from './assets/brand.png'
+import ourStory from './assets/our_story.png'
+
+// Components
+import Button from "./components/button/Button.jsx";
+import ProductCard from "./components/productcard/ProductCard.jsx";
+
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <Button isDisabled={false}>to the collection</Button>
+                <Button isDisabled={false}>shop all bags</Button>
+                <Button isDisabled={true}>pre-orders</Button>
+            </nav>
+            <main>
+                <ProductCard
+                    name={"The handy bag"}
+                    price={400}
+                    label={"Best Seller"}
+                    imageSrc={bag1}
+                />
+
+                <ProductCard
+                    name={"The stylish bag"}
+                    price={250}
+                    label={"Best Seller"}
+                    imageSrc={bag2}
+                />
+
+                <ProductCard
+                    name={"The simple bag"}
+                    price={300}
+                    label={"New Collection"}
+                    imageSrc={bag3}
+                />
+
+                <ProductCard
+                    name={"The trendy bag"}
+                    price={400}
+                    label={"New Collection"}
+                    imageSrc={bag4}
+                />
+            </main>
+            <footer>
+                <section>
+                    <h2>The brand</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus doloremque impedit labore
+                        nulla numquam quia quis quo sequi tempore veritatis!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto commodi dolore dolorem fuga
+                        hic impedit nemo quaerat quam repudiandae similique.</p>
+                </section>
+                <section>
+                    <img src={brand} alt="photo with logo"/>
+                </section>
+                <section>
+                    <img src={ourStory} alt="photo of two women hugging"/>
+                </section>
+                <section>
+                    <h2>Our Story</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur eos iure nisi qui. Commodi
+                        magni minus provident. Dignissimos error ex, explicabo nam nisi qui sed tempora? Amet explicabo
+                        hic placeat!</p>
+                </section>
+            </footer>
+            <Button></Button>
+            <Tile></Tile>
+        </>
+
+    )
 }
 
 export default App
